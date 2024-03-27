02a351015c96286fb32056c7318d3b8d3d4905ac
kevins19 <kevin.j.shan@gmail.com>
2024-03-26 22:03:48 -0400

    setup

--- app/src/main/java/com/example/spotifywrapped/MainActivity.java
+++ app/src/main/java/com/example/spotifywrapped/MainActivity.java
@@ -16,57 +16,82 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.spotifywrapped.databinding.ActivityMainBinding;
 
+//import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
+import java.util.HashMap;
+import android.util.Log;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private Switch theme_switch;
     SharedPreferences sharedPreferences = null;
 
+//    private FirebaseFirestore db;
+    private static final String TAG = "LOG_TAG";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+//        System.out.println("NEEDHELP");
+        Log.d(TAG, "Hello, World!");
+//
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
 
-        // NAV BAR SETUP
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_wrap, R.id.navigation_history, R.id.navigation_update)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
 
-        // THEME SWITCHER SETUP
-        theme_switch = findViewById(R.id.switch_theme);
-        sharedPreferences = getSharedPreferences("night", 0);
-        Boolean switch_theme_flag = sharedPreferences.getBoolean("night_mode", true);
-        if (switch_theme_flag) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-            theme_switch.setChecked(true);
-        }
+//        db = FirebaseFirestore.getInstance();
+//
+//        Map<String, Object> user = new HashMap<>();
+//        user.put("username", "check");
+//        user.put("email", "test@example.com");
+//
+//        db.collection("user-info").add(user);
+//
+//        Log.d(TAG, "Done, World!");
 
-        theme_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                    theme_switch.setChecked(true);
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putBoolean("night_mode", true);
-                    editor.apply();
-                } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                    theme_switch.setChecked(false);
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putBoolean("night_mode", false);
-                    editor.apply();
-                }
-            }
-        });
+
+
+//        // NAV BAR SETUP
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_wrap, R.id.navigation_history, R.id.navigation_update)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(binding.navView, navController);
+//
+//        // THEME SWITCHER SETUP
+//        theme_switch = findViewById(R.id.switch_theme);
+//        sharedPreferences = getSharedPreferences("night", 0);
+//        Boolean switch_theme_flag = sharedPreferences.getBoolean("night_mode", true);
+//        if (switch_theme_flag) {
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//            theme_switch.setChecked(true);
+//        }
+//
+//        theme_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//                    theme_switch.setChecked(true);
+//                    SharedPreferences.Editor editor = sharedPreferences.edit();
+//                    editor.putBoolean("night_mode", true);
+//                    editor.apply();
+//                } else {
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//                    theme_switch.setChecked(false);
+//                    SharedPreferences.Editor editor = sharedPreferences.edit();
+//                    editor.putBoolean("night_mode", false);
+//                    editor.apply();
+//                }
+//            }
+//        });
     }
 
 }

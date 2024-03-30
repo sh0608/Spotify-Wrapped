c939d2cbcdf5ab8c44018fc57dfd8bf8b3a4137b
annabelleslee <soyoonlee0629@gmail.com>
2024-03-30 01:37:50 -0400

    Comments
    comments

--- app/src/main/java/com/example/spotifywrapped/ui/wrap/TopSongAdapter.java
+++ app/src/main/java/com/example/spotifywrapped/ui/wrap/TopSongAdapter.java
@@ -39,35 +39,64 @@ public class TopSongAdapter extends RecyclerView.Adapter<TopSongAdapter.ViewHold
         this.model = model;
     }
 
+    /**
+     * Return the view type of the item at position for the purposes of view recycling.
+     * @param position position to query
+     * @return the view type of the item
+     */
     public int getItemViewType (final int position) {
         return R.layout.item_topsong;
     }
+
+    /**
+     *
+     * @param parent The ViewGroup into which the new View will be added after it is bound to
+     *               an adapter position.
+     * @param viewType The view type of the new View.
+     *
+     * @return viewHolder of each topSong
+     */
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
-        View contactView = inflater.inflate(R.layout.item_topsong, parent, false);
-
-        ViewHolder viewHolder = new ViewHolder(contactView);
+        View topSong = inflater.inflate(R.layout.item_topsong, parent, false);
+        ViewHolder viewHolder = new ViewHolder(topSong);
         return viewHolder;
-
     }
 
+    /**
+     * Binds the data into the xml file.
+     * @param holder The ViewHolder which should be updated to represent the contents of the
+     *        item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Song song = topSongs.get(position);
+
         TextView topSongTitleTextView = holder.topSongTitleTextView;
-        topSongTitleTextView.setText(song.getName());
+        String topSongTitle =  (position + 1) + ". " + song.getName();
+        topSongTitleTextView.setText(topSongTitle);
+
         TextView topSongArtistNameTextView = holder.topSongArtistNameTextView;
         String topArtistsNameFormatted = formatArray(song.getArtists());
         topSongArtistNameTextView.setText(topArtistsNameFormatted);
+
         TextView topSongAlbumTitleTextView = holder.topSongAlbumTitleTextView;
         topSongAlbumTitleTextView.setText(song.getAlbum());
+
         ImageView topSongImageView = holder.topSongImageView;
+        // calls Picasso library to process the imageUrl into ImageView
         Picasso.get().load(song.getImageUrl()).into(topSongImageView);
     }
 
+    /**
+     * private method to process the artists array into a String.
+     * @param arr String array
+     * @return String of artists
+     */
     private String formatArray (String[] arr) {
         String rtn = Arrays.toString(arr);
         rtn = rtn.substring(1, rtn.length() - 1);
@@ -75,16 +104,28 @@ public class TopSongAdapter extends RecyclerView.Adapter<TopSongAdapter.ViewHold
     }
 
 
+    /**
+     * returns the number of topSongs
+     * @return number of topSongs
+     */
     @Override
     public int getItemCount() {
         return topSongs.size();
     }
 
+    /**
+     * setter for topSongs
+     * @param newSongs new list of top songs
+     */
     public void setTopSongs(List<Song> newSongs) {
         topSongs = newSongs;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+
+    /**
+     * inner VieHolder class
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView topSongTitleTextView;
         TextView topSongArtistNameTextView;
         TextView topSongAlbumTitleTextView;
